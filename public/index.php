42852ef3b1d764b94ec60bde2311fd62e3c823b5
Patrik Farkas <farkas.patrik105@gmail.com>
2024-10-28 12:35:05 +0100

    Formats index.php

--- public/index.php
+++ public/index.php
@@ -16,7 +16,13 @@ $sex = "male";
 echo Validator::isEmail($email) ? "Your email address is valid!\n" : "Your email address is not valid!\n";
 echo Validator::isPasswordValid($password, 5, 10) ? "Your password is valid!\n" : "Your password is not valid!\n";
 echo Validator::isAgeValid($age, 20) ? "You are 20 years old!\n" : "You are not 20 years old !\n";
-echo Validator::isAgeBetween($age, 18, "22") ? "Your age is between 18 and 22!\n" : "Your age is not between 18 and 22!\n";
+echo Validator::isAgeBetween(
+    $age,
+    18,
+    "22"
+) ? "Your age is between 18 and 22!\n" : "Your age is not between 18 and 22!\n";
 echo Validator::isAgeExactly($age, 18) ? "You are exactly 18 years old!\n" : "You are not 18 years old!\n";
 echo Validator::isAgeNot($age, 10) ? "You are not 10 years old!\n" : "You are 10 years old !\n";
-echo Validator::isSexValid($sex) ? "You are " . $sex . " which is valid!\n" : "You are " . $sex . " which is not valid!\n";
+echo Validator::isSexValid(
+    $sex
+) ? "You are " . $sex . " which is valid!\n" : "You are " . $sex . " which is not valid!\n";
